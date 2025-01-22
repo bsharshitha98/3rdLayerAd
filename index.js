57f19789e631c69c3c6b435142545989e3b2b763
Harshitha <61576196+bsharshitha98@users.noreply.github.com>
2025-01-22 16:13:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.setHeader("Referrer-Policy", "strict-origin");
   next();
 });
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
